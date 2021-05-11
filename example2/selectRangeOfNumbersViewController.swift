fbd7a6e015b5b3872ab60afe8106a137be69a61d
Alex Boguszewski <Aboguszewski2037@stu.d214.org>
2021-05-11 15:22:06 -0500

    added code to view controllers

---
+++ example2/selectRangeOfNumbersViewController.swift
@@ -0,0 +1,37 @@
+//
+//  selectRangeOfNumbersViewController.swift
+//  example2
+//
+//  Created by AlexBoguszewski on 5/11/21.
+//  Copyright © 2021 AlexBoguszewski. All rights reserved.
+//
+
+import UIKit
+
+class selectRangeOfNumbersViewController: UIViewController {
+
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    var firstNumberEntered : String
+    var secondNumberEntered : String
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        firstNumber.text = (firstNumberEntered)
+        secondNumber.text = (secondNumberEntered)
+        let randomInt = Int.random(in: firstNumberEntered..<secondNumberEntered)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ example2/showRandomNumberViewController.swift
@@ -0,0 +1,31 @@
+//
+//  showRandomNumberViewController.swift
+//  example2
+//
+//  Created by AlexBoguszewski on 5/11/21.
+//  Copyright © 2021 AlexBoguszewski. All rights reserved.
+//
+
+import UIKit
+
+class showRandomNumberViewController: UIViewController {
+
+    @IBOutlet weak var randomNumber: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
